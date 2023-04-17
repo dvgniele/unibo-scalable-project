ce91a9eef5c63f8a1f9dde9623bfb6702629cab9
Stefano Notari <stefano.notari2@studio.unibo.it>
2023-04-17 15:52:27 +0200

    updated location of key file (in the cluster)

--- src/main/scala/hadoopConfigurationBuilder/HadoopConfigurationBuilder.scala
+++ src/main/scala/hadoopConfigurationBuilder/HadoopConfigurationBuilder.scala
@@ -9,7 +9,7 @@ object HadoopConfigurationBuilder {
   private val localSource = s"./dataset/dataset"
 
   def getHadoopConfigurationForGoogleCloudPlatform: Configuration = {
-    val jsonKeyFilePath = "/home/notty/config/key.json"
+    val jsonKeyFilePath = "/home/notty/key.json"
     val hadoopConfiguration = new Configuration()
     hadoopConfiguration.set("fs.AbstractFileSystem.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS")
     hadoopConfiguration.set("fs.gs.project.id", "helloworld-379211")
