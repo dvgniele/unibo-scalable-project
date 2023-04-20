a3e997630b2d041493e2dd20697d5f532101751b
Stefano Notari <stefano.notari2@studio.unibo.it>
2023-04-20 13:25:04 +0200

    replaced KMeans with BisectingKMeans

--- src/main/scala/segmentation/IImageSegmentation.scala
+++ src/main/scala/segmentation/IImageSegmentation.scala
@@ -1,15 +1,15 @@
 package org.br4ve.trave1er
 package segmentation
 
-import org.apache.spark.ml.clustering.KMeansModel
+import org.apache.spark.ml.clustering.{BisectingKMeansModel}
 import org.apache.spark.sql.DataFrame
 
 import java.awt.image.BufferedImage
 
 trait IImageSegmentation {
-	def modelFit(data: DataFrame): KMeansModel
+	def modelFit(data: DataFrame): BisectingKMeansModel
 
-	def transformData(data: DataFrame, model: KMeansModel): DataFrame
+	def transformData(data: DataFrame, model: BisectingKMeansModel): DataFrame
 
 	def getSegmentedImage(data: DataFrame, width: Int, height: Int): BufferedImage
 
